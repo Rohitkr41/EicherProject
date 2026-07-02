2f2f5141baba9970da6b0f3ba7f420799d18ac06
rohit kumar <138009969+Rohitkr41@users.noreply.github.com>
2026-07-02 09:30:19 +0530

    Update DiagnosisAndAdviseTest.java

--- src/test/java/tests/clinicalExamination/DiagnosisAndAdviseTest.java
+++ src/test/java/tests/clinicalExamination/DiagnosisAndAdviseTest.java
@@ -35,7 +35,7 @@ public class DiagnosisAndAdviseTest extends BaseTest {
 
             // ===== OPEN PATIENT =====
             ClinicalPage clinical = new ClinicalPage(driver);
-            clinical.searchByDate("02/04/2026", "03/04/2026");
+            clinical.searchByDate("01/07/2026","30/09/2026");
             clinical.clickNewStatusIcon();
             System.out.println("✅ Patient opened");
 
